a5999f30c4f0aba6d022b3da98a4e89442083a4f
monteirolc <lucascastro47@hotmail.com>
2022-01-07 15:46:24 -0300

    Feat.: Search bar and order by id

--- Agenda/contatos/views.py
+++ Agenda/contatos/views.py
@@ -10,7 +10,9 @@ from django.contrib import messages
 
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('-id').filter(
+        mostrar=True
+    )
     paginator = Paginator(contatos, 5)
     page = request.GET.get('p')
     contatos = paginator.get_page(page)
@@ -20,10 +22,26 @@ def index(request):
 def ver_contato(request, contato_id):
     contato = get_object_or_404(Contato, id=contato_id)
 
-    #if not contato.mostrar:
-    #    raise Http404()
+    if not contato.mostrar:
+        raise Http404()
 
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
     })
 
+def busca(request):
+    termo = request.GET.get('termo')
+
+    if termo is None or not termo:
+        raise Http404()
+    campos = Concat('nome',Value(' '), 'sobrenome')
+    contatos = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo),
+        mostrar=True
+    )
+    paginator = Paginator(contatos, 5)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+    return render(request, 'contatos/busca.html',{ 'contatos': contatos})
